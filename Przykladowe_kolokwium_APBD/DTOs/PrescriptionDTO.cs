2f15f861bfff382f1c7bcf6d8c6cda26d811d9fd
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-05-10 08:14:05 +0200

    create validator for Prescription

--- Przykladowe_kolokwium_APBD/DTOs/PrescriptionDTO.cs
+++ Przykladowe_kolokwium_APBD/DTOs/PrescriptionDTO.cs
@@ -1,11 +1,8 @@
 namespace Kolokwium1_APBD.DTOs;
 
-public class PrescriptionDTO
-{
     public record CreatePrescriptionDto(
         int IdPrescription,
         DateTime Date,
         DateTime DueDate,
         int IdPatient,
         int IdDoctor);
-}
--- Przykladowe_kolokwium_APBD/Validators/PrescriptionValidator.cs
+++ Przykladowe_kolokwium_APBD/Validators/PrescriptionValidator.cs
@@ -3,10 +3,14 @@ using Kolokwium1_APBD.DTOs;
 
 namespace Przykladowe_kolokwium_APBD.Validators;
 
-public class PrescriptionValidator : AbstractValidator<PrescriptionDTO>
+public class PrescriptionValidator : AbstractValidator<CreatePrescriptionDto>
 {
     public PrescriptionValidator()
     {
-        RuleFor(e => e)
+        RuleFor(e => e.IdPrescription).NotNull();
+        RuleFor(e => e.Date).NotNull();
+        RuleFor(e => e.DueDate).NotNull();
+        RuleFor(e => e.IdPatient).NotNull();
+        RuleFor(e => e.IdDoctor).NotNull();
     }
 }
